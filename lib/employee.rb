958cfc5f67b9b7099a2ccc2a2dc87b5bd35c842c
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-07 20:59:06 -0500

    testing files

--- lib/employee.rb
+++ lib/employee.rb
@@ -8,14 +8,23 @@ class Employee < ActiveRecord::Base
   validate :hourly_rate, :in_range
   validates_associated :store
 
-  before_validation :generate_password, on: :create
+  # before_validation :generate_password, on: :create
 
   def in_range
     wage = hourly_rate.to_i
     errors.add(:hourly_rate, 'Must be between 40 and 200') unless wage >= 40 && wage <= 200
   end
 
-  def generate_password
+  # def generate_password
+  #   self.password = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+  # end
+
+  # after_create do
+  #   self.password = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+  #   save
+  # end
+
+  before_save do
     self.password = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
   end
 end
