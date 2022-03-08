5c63f3f7684b29858f0bdbb902c3cdf429840991
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-07 19:47:27 -0500

    finish excercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,15 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+total = Store.sum(:annual_revenue)
+puts "Total Revenue: #{total}"
+
+average = total / Store.count
+puts "Average Revenue #{average}"
+
+high_earners = Store.where('annual_revenue >= ?', 1_000_000)
+puts "Stores earning over 1M: #{high_earners.count}"
