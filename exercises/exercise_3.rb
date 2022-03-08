b6798d4815c90a84e19edb045e7d59342f9fa122
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-07 19:30:37 -0500

    finished excercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,10 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
-puts "Exercise 3"
-puts "----------"
+puts 'Exercise 3'
+puts '----------'
 
 # Your code goes here ...
+Store.find_by(id: 3).destroy
+
+puts "Stores Left: #{Store.count}"
